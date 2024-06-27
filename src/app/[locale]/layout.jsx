ab1d7ff631e375f6e30ea3f00063e346ed0801d2
Damien Cassou <damien@cassou.me>
2024-06-27 10:27:40 +0200

    Initial commit

---
+++ src/app/[locale]/layout.jsx
@@ -0,0 +1,20 @@
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+
+export const metadata = {
+  title: "Country",
+};
+
+export default async function RootLayout({ children, params: {locale} }) {
+  const messages = await getMessages();
+
+  return (
+      <html lang={locale}>
+        <body>
+            <NextIntlClientProvider messages={messages}>
+                {children}
+            </NextIntlClientProvider>
+        </body>
+    </html>
+  );
+}
